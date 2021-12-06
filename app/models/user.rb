f6782e7a7cb89fbece8a28042b3c24b906ceebeb
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-06 18:04:41 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         
+         validates :encrypted_password,:password,:password_confirmation,length:{minimum:7},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+         validates :nickname, presence: true
+         validates :last_name, presence: true
+         validates :fast_name, presence: true
+         validates :kana_last, presence: true
+         validates :kana_fast, presence: true
+         validates :birthday, presence: true
+
+         has_many :items
+         has_many :purchases
+
+
+
+
+
+ end
