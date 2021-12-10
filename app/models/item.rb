af44571b13318390c8059d3fecb18b70a5a9bc2e
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-10 11:13:54 +0900

    モデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,29 +2,22 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   
   belongs_to :user
-  has_one :purchase
-
   has_one_attached :image
-  
-  belongs_to :category
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :days
 
-  #空の投稿を保存できないようにする
-  validates :name, presence: true
-  validates :explanation, presence: true
-  validates :price, presence: true
-  validates :user, presence: true
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :explanation, length: { maximum: 1000 }
+    validates :price, numericality: { only_integer:true, greater_than: 299, less_than: 10_000_000 }
+    validates :image, :category, :status, :shipping_charge, :shipment_source, :days
+    validates :category_id, :status_id, :shipping_charge_id, :shipment_source_id, :days_id, numericality: { other_than: 1}
 
-  
-  
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipment_source_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :days_id, numericality: { other_than: 1 , message: "can't be blank"}
-  
-  
-  
-  
+
+  end
   
   
   
