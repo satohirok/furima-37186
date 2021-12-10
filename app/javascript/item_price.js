562a42258b4d33380c0e2f0556681458a2b79efb
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-10 14:34:08 +0900

    value属性を取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,9 @@ window.addEventListener('load', () => {
   console.log("OK");
   const priceInput = document.getElementById( "item-price" );
   priceInput.addEventListener("input", () => {
-  console.log("イベント発火");
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+
   })
 
 });
