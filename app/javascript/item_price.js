f3f5ff756aac1ddd715b06c4a78a66eb78ec8065
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-10 14:29:48 +0900

    イベント発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
   console.log("OK");
   const priceInput = document.getElementById( "item-price" );
-  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+  console.log("イベント発火");
+  })
+
 });
 
