548d946bf43584338f0b3bae29bb9483fc05a73e
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-13 09:17:11 +0900

    updateアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
 
-  resources :items, only: [:index, :new, :create, :show, :edit]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
 end
