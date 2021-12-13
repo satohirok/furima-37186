4f355098cde27b620ead650fb6b28c6d19ea844c
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-13 12:27:13 +0900

    destroyアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
- before_action :set_item, only: [:show, :edit, :update]
+ before_action :set_item, only: [:show, :edit, :update, :destroy]
  before_action :move_to_index, except: [:index, :show]
  
  def index
@@ -33,6 +33,14 @@ class ItemsController < ApplicationController
     redirect_to item_path(@item.id)
   else
     render :edit
+  end
+ end
+
+ def destroy
+  if @item.destroy
+    redirect_to root_path
+  else
+    render :show
   end
  end
 
