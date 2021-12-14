c1bb93e538b6da0e3417f0e1204c97bc13bd921d
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-14 16:02:20 +0900

    フォームの情報を保存機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,5 +5,25 @@ class PurchasesController < ApplicationController
  end
 
  def create
- end
+  @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+     @purchase_address.save
+     redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture, :municipalities, :house_number, :building, :tel_number, :purchase).merge(user_id: current_user.id)
+  end
+
+
+
+
+
+
+
 end
