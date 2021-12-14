f072ea9a2cf1425f64251104a502b3df36a21b1c
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-14 16:14:16 +0900

    購入テーブル、宛先テーブル作成

--- db/migrate/20211214053710_create_purchases.rb
+++ db/migrate/20211214053710_create_purchases.rb
@@ -1,7 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.references :user,  foreign_key: true, null: false
+      t.references :item,  foreign_key: true, null: false
       t.timestamps
     end
   end
--- db/migrate/20211214053728_create_addresses.rb
+++ db/migrate/20211214053728_create_addresses.rb
@@ -1,6 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
+      t.string :postal_code,                         null: false
+      t.integer :prefectures_id,                     null: false
+      t.string :municipalities,                      null: false
+      t.string :house_number,                        null: false
+      t.string :building,                            null: false
+      t.string :tel_number,                          null: false
+      t.references :purchase,     foreign_key: true, null: false
 
       t.timestamps
     end
