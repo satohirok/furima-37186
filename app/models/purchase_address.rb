71838eb51e8d0c09014f3e1656356a753f01391f
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-14 18:21:36 +0900

    エラー文2

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,10 +3,11 @@ class PurchaseAddress
   attr_accessor :postal_code, :shipment_source_id, :municipalities, :house_number, :building, :tel_number,  :purchase, :user, :item
 
   with_options presence: true do
-    validates :user
-    validates :postal_code, :tel_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+     validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+     validates :user, :municipalities, :house_number
   end
-  validates :shipment_source_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :shipment_source_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :tel_number, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
 
 
   def save
