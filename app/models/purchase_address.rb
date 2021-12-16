d5ce20c9f271146f68e422f58e1716f4362ef61a
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-16 15:31:14 +0900

    バリデーション、テストコードの実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -4,7 +4,7 @@ class PurchaseAddress
 
   with_options presence: true do
      validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-     validates :house_number, :item_id, :user_id, :municipalities
+     validates :house_number, :item_id, :user_id, :municipalities, :token
   end
   validates :shipment_source_id, numericality: {other_than: 1, message: "can't be blank"}
   validates :tel_number, presence: true, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -17,51 +17,66 @@ RSpec.describe PurchaseAddress, type: :model do
     end
 
     context '内容に問題がある場合' do
+      
       it 'postal_codeが空だと保存できないこと' do
         @purchase_address.postal_code = ''
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include("Postal code can't be blank")
       end
+      
       it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
         @purchase_address.postal_code = '1234567'
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
       end
+      
       it 'shipment_sourceを選択していないと保存できないこと' do
         @purchase_address.shipment_source_id = ''
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include("Shipment source can't be blank")
       end
+      
       it 'municipalitiesが空だと保存できないこと' do
         @purchase_address.municipalities = ''
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include("Municipalities can't be blank")
       end
+      
       it 'house_numberが空だと保存できないこと' do
         @purchase_address.house_number = ''
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include("House number can't be blank")
       end
+      
       it 'tel_numberが空だと保存できないこと' do
         @purchase_address.tel_number = ''
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include("Tel number can't be blank")
       end
+      
       it 'tel_numberが12桁以上では登録できない' do
         @purchase_address.tel_number = '028242334233'
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include('Tel number is invalid')
       end
-    it 'user_id が空では登録できない' do
+      
+      it 'user_id が空では登録できない' do
        @purchase_address.user_id = ''
        @purchase_address.valid?
        expect(@purchase_address.errors.full_messages).to include("User can't be blank")
-     end
-     it 'item_id が空では登録できない' do
-      @purchase_address.item_id = ''
+      end
+     
+      it 'item_id が空では登録できない' do
+       @purchase_address.item_id = ''
+       @purchase_address.valid?
+       expect(@purchase_address.errors.full_messages).to include("Item can't be blank")
+      end
+     
+     it "tokenが空では登録できないこと" do
+      @purchase_address.token = nil
       @purchase_address.valid?
-      expect(@purchase_address.errors.full_messages).to include("Item can't be blank")
-    end
-    end
+      expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
+     end
+   end
   end
 end
