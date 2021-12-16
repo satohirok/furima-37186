ba1ab22122212bb249eb86d3f51cb26823dda7cb
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-16 14:45:32 +0900

    purchase_addressモデルにトークンの値を追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :shipment_source_id, :municipalities, :house_number, :building, :tel_number,  :purchase_id, :user_id, :item_id
+  attr_accessor :postal_code, :shipment_source_id, :municipalities, :house_number, :building, :tel_number,  :purchase_id, :user_id, :item_id, :token
 
   with_options presence: true do
      validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
