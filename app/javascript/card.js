72e73f986a44e537a5c845222ad5e2346aa9f634
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-16 11:11:50 +0900

    PAY.JPのテスト公開鍵を取得し設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,9 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_48fc75cabdeae2b0d5f26dbb"); // PAY.JPテスト公開鍵
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    
   });
 };
 
