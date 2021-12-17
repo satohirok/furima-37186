11b65438fb162cdc564f7b7a802504bceb216558
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-17 12:18:41 +0900

    purchaseコントローラ修正（addressが保存できなかったため））

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,8 @@
 class PurchasesController < ApplicationController
- 
+  
+  before_action :move_to_index
+  before_action :get_item_infomation, only: [:index, :create]
+
  def index
   @purchase_address = PurchaseAddress.new
  end
@@ -7,7 +10,8 @@ class PurchasesController < ApplicationController
  def create
   @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
-     @purchase_address.save
+      pay_item
+      @purchase_address.save
      redirect_to root_path
     else
       render :index
@@ -15,9 +19,32 @@ class PurchasesController < ApplicationController
   end
 
   private
+  
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if (user_signed_in? && current_user.id == @item.user_id) || @item.purchase.present?
+      redirect_to root_path
+    elsif user_signed_in? == false
+      redirect_to new_user_session_path 
+    end
+  end
+
+  def get_item_infomation
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :shipment_source_id, :municipalities, :house_number, :building, :tel_number).merge(user_id: current_user.id token: params[:token])
+    params.require(:purchase_address).permit(:postal_code, :shipment_source_id, :municipalities, :house_number, :building, :tel_number).merge(user_id: current_user.id, token: params[:token],item_id: params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],    
+      currency: 'jpy'                 
+    )
+  end
+
 
 end
