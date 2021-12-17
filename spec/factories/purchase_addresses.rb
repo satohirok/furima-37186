0c07996e30607b2fa70f17c2f8afca35526956bd
satohirok <hiroki_g0421@yahoo.co.jp>
2021-12-17 14:29:22 +0900

    修正

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     postal_code { '123-4567' }
     shipment_source_id { Faker::Number.between(from: 2, to: 48) }
     municipalities { '江東区' }
+    building{ '森ビル111' }
+    tel_number{ '08012345678' }
     item_id                   { '1' }
     token{"tok_abcdefghijk00000000000000000"}
   end
